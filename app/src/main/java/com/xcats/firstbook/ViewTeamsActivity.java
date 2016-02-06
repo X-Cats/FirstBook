825acb58b094a7da7c8ad6f2fc0277b23c13bd7e
aidan <2001flyingpigs14@gmail.com>
2016-02-06 14:54:44 -0500

    Set up Subteams list, as well as transition to it from teams list

--- app/src/main/java/com/xcats/firstbook/ViewTeamsActivity.java
+++ app/src/main/java/com/xcats/firstbook/ViewTeamsActivity.java
@@ -1,7 +1,9 @@
 package com.xcats.firstbook;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by aidan on 2/6/16.
@@ -10,5 +12,9 @@ public class ViewTeamsActivity extends Activity {
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_team_list);
+    }
+    public void viewSubTeams (View view){
+        Intent viewSubTeams = new Intent(this,ViewSubTeamsActivity.class);
+        this.startActivity(viewSubTeams);
     }
 }
