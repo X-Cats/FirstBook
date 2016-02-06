7ed4838cffe81bd00a25966daff2cabe1ed55b6f
aidan <2001flyingpigs14@gmail.com>
2016-02-06 16:26:47 -0500

    Set up members list and added transition from subteams list to it. Set up profile view and added transition from members list to it. Set up second step of Create Profile and added transition from step one to it.

--- app/src/main/java/com/xcats/firstbook/CreateProfileActivity2.java
+++ app/src/main/java/com/xcats/firstbook/CreateProfileActivity2.java
@@ -2,6 +2,7 @@ package com.xcats.firstbook;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by aidan on 2/6/16.
@@ -10,5 +11,8 @@ public class CreateProfileActivity2 extends Activity {
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_profile2);
+    }
+    public void create (View view){
+
     }
 }
